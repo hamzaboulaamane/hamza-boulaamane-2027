5df38ea825a886e9b9ae551c2a48dfe436b01883
hamzaboulaamane <hamzaboulaamane06@gmail.com>
2026-08-06 22:28:51 +0000

    Add global design system and base styles

---
+++ js/typing.js
@@ -0,0 +1,25 @@
+new Typed("#typing",{
+
+strings:[
+
+"Cybersecurity Student",
+
+"AI & Machine Learning Enthusiast",
+
+"Ethical Hacker",
+
+"Future Cybersecurity Engineer",
+
+"Threat Detection Developer"
+
+],
+
+typeSpeed:60,
+
+backSpeed:35,
+
+backDelay:1800,
+
+loop:true
+
+});
